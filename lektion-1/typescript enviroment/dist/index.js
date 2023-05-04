f18956640d90edade5737df82d09a283bbfd0ecc
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2023-05-04 13:19:38 +0200

    update to lesson 1

--- lektion-1/typescript enviroment/dist/index.js
+++ lektion-1/typescript enviroment/dist/index.js
@@ -2,4 +2,4 @@ function addNumbers(a, b) {
     return a + b;
 }
 var result = addNumbers(1, 2);
-console.log(result);
+console.log(result === null || result === void 0 ? void 0 : result.toExponential());
--- lektion-1/typescript enviroment/src/index.ts
+++ lektion-1/typescript enviroment/src/index.ts
@@ -2,9 +2,9 @@ type User = {
     name: string;
 };
 
-function addNumbers(a: number, b: number): number {
+function addNumbers(a: number, b: number): number | undefined {
     return a + b;
   }
   
   const result = addNumbers(1, 2);
-  console.log(result)
+  console.log(result?.toExponential());
