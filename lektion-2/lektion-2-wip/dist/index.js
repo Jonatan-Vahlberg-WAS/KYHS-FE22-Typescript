6ddec0456025deaef5740883687dfb53d3d9bcc5
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2023-08-25 13:28:01 +0200

    created a task

--- lektion-2/lektion-2-wip/dist/index.js
+++ lektion-2/lektion-2-wip/dist/index.js
@@ -25,3 +25,66 @@ else {
 // const movie: Movie = {
 // }
 console.log((_a = _person.children) === null || _a === void 0 ? void 0 : _a.length);
+var myTuple = [
+    "hej", 12, true
+];
+var greeting = myTuple[0], cost = myTuple[1], bool = myTuple[2];
+//Enum implicit
+var Weekday;
+(function (Weekday) {
+    Weekday[Weekday["NA"] = 0] = "NA";
+    Weekday[Weekday["MONDAY"] = 1] = "MONDAY";
+    Weekday[Weekday["TUESDAY"] = 2] = "TUESDAY";
+    Weekday[Weekday["WEDNESDAY"] = 3] = "WEDNESDAY";
+    Weekday[Weekday["THURSDAY"] = 4] = "THURSDAY";
+    Weekday[Weekday["FRIDAY"] = 5] = "FRIDAY";
+})(Weekday || (Weekday = {}));
+//Enum explicit
+var Color;
+(function (Color) {
+    Color[Color["RED"] = 1] = "RED";
+    Color[Color["GREEN"] = 2] = "GREEN";
+    Color[Color["BLUE"] = 3] = "BLUE";
+})(Color || (Color = {}));
+//Enum String
+var Keys;
+(function (Keys) {
+    Keys["ENTER"] = "ENTER";
+    Keys["DEL"] = "DEl";
+    Keys["SPACE"] = "SPACE";
+})(Keys || (Keys = {}));
+//Computed enum values
+var FileAccess;
+(function (FileAccess) {
+    FileAccess[FileAccess["NA"] = 0] = "NA";
+    FileAccess[FileAccess["READ"] = 1] = "READ";
+    FileAccess[FileAccess["WRITE"] = 2] = "WRITE";
+    FileAccess[FileAccess["READ_WRITE"] = 3] = "READ_WRITE";
+})(FileAccess || (FileAccess = {}));
+var StatusCode;
+(function (StatusCode) {
+    StatusCode[StatusCode["OK"] = 200] = "OK";
+    StatusCode[StatusCode["CREATED"] = 201] = "CREATED";
+    StatusCode[StatusCode["UNALLOWED"] = 401] = "UNALLOWED";
+    StatusCode[StatusCode["NOT_FOUND"] = 404] = "NOT_FOUND";
+})(StatusCode || (StatusCode = {}));
+var RequestType;
+(function (RequestType) {
+    RequestType[RequestType["NA"] = 0] = "NA";
+    RequestType[RequestType["POST"] = 1] = "POST";
+    RequestType[RequestType["GET"] = 2] = "GET";
+    RequestType[RequestType["PUT"] = 3] = "PUT";
+})(RequestType || (RequestType = {}));
+var database = "http://sim.data/api/v1";
+var downDatabase = "http://sim.data.down/api/v1";
+var databaseEndpoints = [
+    "users/1",
+    "users/12",
+    "users/18"
+];
+function simulatedNetworkRequest() {
+    //todo requests have to have a RequestMethod a db and a endpoint
+    //todo requests landing in the down database should return 500
+    //todo requests landing in the normal db has to correspond with an endpoint otherwise 404
+    //todo POST generate 201 GET 200 and any attempt at interacting with user 1 results in 401
+}
--- lektion-2/lektion-2-wip/src/index.ts
+++ lektion-2/lektion-2-wip/src/index.ts
@@ -54,3 +54,73 @@ else{
 console.log(_person.children?.length)
 
 
+const myTuple: [string, number, boolean] = [
+    "hej", 12, true
+]
+
+const [greeting, cost, bool] = myTuple
+
+//Enum implicit
+enum Weekday {
+    NA,
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY
+}
+
+//Enum explicit
+enum Color {
+    RED = 1,
+    GREEN = 2,
+    BLUE = 3
+}
+
+//Enum String
+enum Keys {
+    ENTER = "ENTER",
+    DEL = "DEl",
+    SPACE = "SPACE"
+}
+
+//Computed enum values
+enum FileAccess {
+    NA,
+    READ = 1,
+    WRITE = 2,
+    READ_WRITE = READ | WRITE
+}
+
+enum StatusCode {
+    OK = 200,
+    CREATED = 201,
+    UNALLOWED = 401,
+    NOT_FOUND = 404,
+}
+
+enum RequestType {
+    NA,
+    POST,
+    GET,
+    PUT
+}
+
+const database = "http://sim.data/api/v1";
+const downDatabase = "http://sim.data.down/api/v1"
+
+const databaseEndpoints = [
+    "users/1",
+    "users/12",
+    "users/18"
+]
+
+function simulatedNetworkRequest(){
+    //todo requests have to have a RequestMethod a db and a endpoint
+
+    //todo requests landing in the down database should return 500
+
+    //todo requests landing in the normal db has to correspond with an endpoint otherwise 404
+
+    //todo POST generate 201 GET 200 and any attempt at interacting with user 1 results in 401
+}
