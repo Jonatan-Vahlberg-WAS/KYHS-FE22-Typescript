f979332d0d7684c30b880f0574cf2e729a2c63cd
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2023-08-31 10:29:44 +0200

    generics

---
+++ lektion-3/lektion-3-wip-react/src/Generics.tsx
@@ -0,0 +1,63 @@
+import { useState } from "react"
+
+interface Friend {
+    name: string;
+}
+
+const greeting = (message: string) => {
+    return message
+}
+
+function greetingWithData<T> (message: string, data: T) {
+    
+    message = `${message}${data}`
+
+    return message
+}
+
+// function addData<T> (data1: T, data2: T) {
+//     return data1 + data2
+// }
+
+function sortProducts<T>(products: T[], sortFunction: (product1: T, product2: T) => number) {
+    return products.sort(sortFunction)
+}
+
+const Generics = () => {
+    // Implicit generic useState
+    const [name, setName] = useState("name")
+
+    //explicit generic useState
+    const [age, setAge] = useState<number>(0)
+
+    const friendIds = [1,38,23,12]
+
+    const friends: Friend[] = [
+        { name: "Jane" },
+        { name: "Mark"},
+        { name: "Albert"},
+        { name: "Moa"}
+    ]
+
+    const people
+
+    return <div>
+        Generics
+
+        <p>Greeting: {greeting("Hello world")}</p>
+
+        <p>Greeting with data: {greetingWithData<number>("Today is the ", 31)}</p>
+        <p>Greeting with data: {greetingWithData("It is raining today ", false)}</p>
+
+        <p> Sorted friend ids:&nbsp;
+            {sortProducts<number>(friendIds, (id1, id2) => id1 - id2).join(", ")}
+        </p>
+
+        <p>
+            Sorted friends:&nbsp;
+            {sortProducts<Friend>(friends,(f1, f2) => f1.name.length - f2.name.length).map(f => f.name).join(", ")}
+        </p>
+    </div>
+}
+
+export default Generics
---
+++ lektion-3/lektion-3-wip-react/src/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import Generics from './Generics';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+// Q: when did destructuring assignment get added to JS?
+// A: ES6
+const unDestructured = { a: 1, b: 2 };
+const { a, b } = unDestructured;
+
+const unDestructuredArray = [1, 2, 3];
+const [first, second, third] = unDestructuredArray;
+
+root.render(
+  <React.StrictMode>
+    <Generics/>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
