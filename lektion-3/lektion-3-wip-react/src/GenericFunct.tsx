c825cf9567261d4c617395fe5364288d7d22936e
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2023-08-31 10:50:46 +0200

    wip

--- lektion-3/lektion-3-wip-react/src/GenericFunct.tsx
+++ lektion-3/lektion-3-wip-react/src/GenericFunct.tsx
@@ -2,13 +2,14 @@
 
 //Generate a generic task that uses the following: generic functions and arguments
 
-// 1. Create a generic function that takes in a value of type and returns 
+// 1. Create a generic function that takes in a value of type T and returns 
     // that value
-    
+
 // 2. Create a generic function that takes in a value of type T and a 
     // array of type T and returns the array with the value added to it
 
-// 3. Create a generic function that takes in a array of type T and designation of type T and returns a array of arrays of type T
+// 3. Create a generic function that takes in a array of type T and search value of
+    // type T and returns a tuple of two arrays.
     // 3.1. splits the array at a designated value of type T
 
 const GenericsFunct = () => {
